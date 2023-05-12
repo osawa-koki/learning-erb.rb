c10b5cbdaeeb4f06285e9586941ec09d06ca6fa5
osawa-koki <github@osawa.dev>
2023-05-13 00:25:19 +0900

    ERB使ってみた。

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'erb'
+
+class Main
+
+  def initialize
+    @title = "Hello World"
+    @fruits = ["apple", "banana", "orange", "grape", "strawberry"]
+    @current_hour = Time.now.hour
+  end
+
+  def put_description
+    "This is a description."
+  end
+
+  def Main
+    template_file = File.read('template.erb')
+    template = ERB.new(template_file)
+    output = template.result(binding)
+    puts output
+  end
+
+end
+
+Main.new.Main
